88bbc00123758ab7cb34fc8775f97650a7b58455
likeroge <likeroge@gmail.com>
2025-09-18 18:32:23 +0400

    update cors

--- occportal/src/main/java/ru/avia/occportal/configuration/WebConfig.java
+++ occportal/src/main/java/ru/avia/occportal/configuration/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
